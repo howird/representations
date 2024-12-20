f689f77590dc456385d5e63b2e348cc0e7a7f454
Howard Nguyen-Huu <howard.nguyen-huu@uwaterloo.ca>
2024-12-20 10:06:23 -0500

    feat(v2): add more fc layers, definable feature_dim

--- representations/models/semisupclassifier.py
+++ representations/models/semisupclassifier.py
@@ -17,14 +17,24 @@ class SemiSupervisedClassifier(nn.Module):
         backbone: nn.Module,
         feature_dim: int,
         writer: SummaryWriter,
+        hidden_sizes: list[int] = [256, 128],
     ):
-        super().__init__()
+        super(SemiSupervisedClassifier, self).__init__()
         self.num_classes = num_classes
         self.feature_dim = feature_dim
         self.writer = writer
 
         self.backbone = backbone
-        self.classifier = nn.Linear(self.feature_dim, self.num_classes)
+
+        layer_sizes = [feature_dim] + hidden_sizes + [num_classes]
+
+        layers = []
+        for i in range(len(layer_sizes) - 1):
+            layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
+            if i < len(layer_sizes) - 2:
+                layers.append(nn.ReLU())
+
+        self.classifier = nn.Sequential(*layers)
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
